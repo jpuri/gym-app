0a0ee8e8697a98492846d34626d5c592f2c73394
Jyoti Puri <jyotipuri@gmail.com>
2026-05-14 11:06:23 +0530

    adding build

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   turbopack: {
     root: path.resolve(__dirname),
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
